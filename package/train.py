22885ba7da183ee49af63f0832b62e926bb1be85
YuChia <erwin.yuchia.chen@gmail.com>
2017-01-16 14:39:26 -0800

    OOPize train.py

--- package/train.py
+++ package/train.py
@@ -4,33 +4,32 @@ import math
 
 START_SYMBOL = '*'
 STOP_SYMBOL = 'STOP'
+MAXIMUM_TRAIN_N_GRAM = 3
 
 def train_language_models(corpus):
-    unigrams_full_list = []
-    unigrams_list_no_start = []
-    bigrams_full_list = []
-    bigrams_list_2_start = []
-    trigrams_full_list = []
+    ngram_result = []
+    for n in range(MAXIMUM_TRAIN_N_GRAM):
+        n_gram_freq_dist,n_1_gram_freq_dist = train_ngram_model(corpus,n+1)
+        if n == 0:
+            ngram_result.append({k: math.log( float(v)/n_1_gram_freq_dist, 2) for k,v in n_gram_freq_dist.items()})
+        else:
+            ngram_result.append({k: math.log( float(v)/n_1_gram_freq_dist[tuple(k[:n])], 2) for k,v in n_gram_freq_dist.items()})
+    return ngram_result[0],ngram_result[1],ngram_result[2]
+
+def train_ngram_model(corpus, n):
+    n_gram_list = []
+    n_1_gram_list = []
     for sentence in corpus:
         tokens = sentence.split(' ')
-        tokens.append(STOP_SYMBOL)
-        unigrams_list_no_start.extend(tokens)
-        tokens.insert(0,START_SYMBOL)
-        unigrams_full_list.extend(tokens)
-        bigrams_full_list.extend(list(nltk.bigrams(tokens)))
+        tokens = [START_SYMBOL for i in range(n-1)]+tokens+[STOP_SYMBOL]
+        n_gram_model = nltk.ngrams(tokens,n)
+        if n != 1:
+            n_1_gram_model = nltk.ngrams(tokens,n-1)
+            n_1_gram_list.extend(n_1_gram_model)
 
-        tokens.insert(0,START_SYMBOL)
-        bigrams_list_2_start.extend(list(nltk.bigrams(tokens)))
-        trigrams_full_list.extend(list(nltk.trigrams(tokens)))
+        n_gram_list.extend(n_gram_model)
 
-    uni_fdist = dict(nltk.FreqDist(unigrams_full_list))
-    uni_fdist_no_start = dict(nltk.FreqDist(unigrams_list_no_start))
-    bi_fdist = dict(nltk.FreqDist(bigrams_full_list))
-    bi_fdist_2_start = dict(nltk.FreqDist(bigrams_list_2_start))
-    tri_fdist = dict(nltk.FreqDist(trigrams_full_list))
-
-    unigram_p = {(k,): math.log( float(v)/len(unigrams_list_no_start), 2) for k,v in uni_fdist_no_start.items()}
-    bigram_p = {k: math.log( float(v)/uni_fdist[k[0]], 2) for k,v in bi_fdist.items()}
-    trigram_p = {k: math.log( float(v)/bi_fdist_2_start[(k[0],k[1])], 2) for k,v in tri_fdist.items()}
-
-    return unigram_p, bigram_p, trigram_p
+    if n != 1:
+        return dict(nltk.FreqDist(n_gram_list)), dict(nltk.FreqDist(n_1_gram_list))
+    else:
+        return dict(nltk.FreqDist(n_gram_list)), len(n_gram_list)
