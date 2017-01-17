eef80ef9c36f0fdf60cb14789c5a1ae150b2b051
YuChia <erwin.yuchia.chen@gmail.com>
2017-01-16 16:48:01 -0800

    Finish freq dist

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from package.load import load_corpus
 from package.train import train_language_models
 from package.evaluation import calc_perplexity
+import time
 
 CORPUS_PATH = './prob1_brown_full/'
 TRAINING_CORPUS = CORPUS_PATH+'brown.train.txt'
@@ -8,6 +9,7 @@ DEV_CORPUS = CORPUS_PATH+'brown.dev.txt'
 TEST_CORPUS = CORPUS_PATH+'brown.test.txt'
 
 def main():
+    start = time.time()
     try:
         training_corpus = load_corpus(TRAINING_CORPUS)
         unigram_p, bigram_p, trigram_p = train_language_models(training_corpus)
@@ -15,6 +17,7 @@ def main():
         dev_corpus = load_corpus(DEV_CORPUS)
         perplexity = calc_perplexity(dev_corpus, bigram_p, 2)
         print perplexity
+        print time.time()-start
 
     except RuntimeError as error:
         print error
--- package/frequency_distribution.py
+++ package/frequency_distribution.py
@@ -1,4 +1,10 @@
-import nltk
+# import nltk
+from collections import defaultdict
 
 def calc_freq_dist(lm_list):
-    return dict(nltk.FreqDist(lm_list))
+    dictionary = defaultdict(int)
+    for lm in lm_list:
+        dictionary[lm] += 1
+    return dictionary
+    # return {lm: lm_list.count(lm) for lm in lm_list}
+    # return dict(nltk.FreqDist(lm_list))
